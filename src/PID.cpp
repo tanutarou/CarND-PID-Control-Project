1185d14b36ffa2bceffd23ac08c81dea88c55e72
tanutarou <tanutarou@gmail.com>
2018-11-13 02:51:17 +0900

    [WIP] cannot implement twiddle

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::p_error = 0;
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    PID::d_error = PID::p_error - cte;
+    PID::p_error = cte;
+    PID::i_error += cte;
 }
 
 double PID::TotalError() {
+    return PID::Kp * PID::p_error + PID::Kd * PID::d_error + PID::Ki * PID::i_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,15 @@
 
 // for convenience
 using json = nlohmann::json;
+// global
+size_t runtime = 0;
+double err = 0;
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+double run(PID pid);
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -28,12 +32,45 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
-{
-  uWS::Hub h;
+void twiddle(double tol=1e-5){
+    double p[3] = {0.0, 0.0, 0.0};
+    double dp[3] = {1.0, 1.0, 1.0};
+    PID pid;
+    pid.Init(p[0], p[1], p[2]);
+    runtime = 0; err = 0;
+    double best_err = run(pid);
+    /*
 
-  PID pid;
-  // TODO: Initialize the pid variable.
+    while(dp[0] + dp[1] + dp[2] > tol){
+        for(int i=0; i<3; i++){
+            p[i] += dp[i];
+            pid.Init(p[0], p[1], p[2]);
+            runtime = 0; err = 0;
+            double err = run(pid);
+            if(err < best_err){
+                best_err = err;
+                dp[i] *= 1.1;
+            }else{
+                p[i] -= 2*dp[i];
+                pid.Init(p[0], p[1], p[2]);
+                runtime = 0; err = 0;
+                double err = run(pid);
+                if(err < best_err){
+                    best_err = err;
+                    dp[i] *= 1.1;
+                }else{
+                    p[i] += dp[i];
+                    dp[i] *= 0.9;
+                }
+            }
+        }
+        printf("error %f: %f %f %f\n", best_err, p[0], p[1], p[2]);
+    }
+    */
+}
+
+double run(PID pid){
+  uWS::Hub h;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -41,6 +78,13 @@ int main()
     // The 2 signifies a websocket event
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
+      printf("runtime %d\n", runtime);
+      runtime++;
+      if(runtime == 100){
+          std::string msg = "42[\"reset\",{}]";
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          return err;
+      }
       auto s = hasData(std::string(data).substr(0, length));
       if (s != "") {
         auto j = json::parse(s);
@@ -51,6 +95,12 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
+          err += cte;
+
+          pid.UpdateError(cte);
+          steer_value = -pid.TotalError();
+          if(steer_value < -1) steer_value = -1;
+          if(steer_value > 1) steer_value = 1;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
@@ -67,6 +117,7 @@ int main()
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
         }
       } else {
         // Manual driving
@@ -111,4 +162,101 @@ int main()
     return -1;
   }
   h.run();
+}
+
+int main()
+{
+  const bool TWIDDLE = true;
+  if(TWIDDLE){
+    twiddle(1e-5);
+
+  }else{
+      uWS::Hub h;
+
+      PID pid;
+      // TODO: Initialize the pid variable.
+      pid.Init(0.1, 0.001, 0.01);
+
+      h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+        // "42" at the start of the message means there's a websocket message event.
+        // The 4 signifies a websocket message
+        // The 2 signifies a websocket event
+        if (length && length > 2 && data[0] == '4' && data[1] == '2')
+        {
+          auto s = hasData(std::string(data).substr(0, length));
+          if (s != "") {
+            auto j = json::parse(s);
+            std::string event = j[0].get<std::string>();
+            if (event == "telemetry") {
+              // j[1] is the data JSON object
+              double cte = std::stod(j[1]["cte"].get<std::string>());
+              double speed = std::stod(j[1]["speed"].get<std::string>());
+              double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+              double steer_value;
+
+              pid.UpdateError(cte);
+              steer_value = -pid.TotalError();
+              if(steer_value < -1) steer_value = -1;
+              if(steer_value > 1) steer_value = 1;
+              /*
+              * TODO: Calcuate steering value here, remember the steering value is
+              * [-1, 1].
+              * NOTE: Feel free to play around with the throttle and speed. Maybe use
+              * another PID controller to control the speed!
+              */
+              
+              // DEBUG
+              std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+
+              json msgJson;
+              msgJson["steering_angle"] = steer_value;
+              msgJson["throttle"] = 0.3;
+              auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+              std::cout << msg << std::endl;
+              ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            }
+          } else {
+            // Manual driving
+            std::string msg = "42[\"manual\",{}]";
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          }
+        }
+      });
+
+      // We don't need this since we're not using HTTP but if it's removed the program
+      // doesn't compile :-(
+      h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+        const std::string s = "<h1>Hello world!</h1>";
+        if (req.getUrl().valueLength == 1)
+        {
+          res->end(s.data(), s.length());
+        }
+        else
+        {
+          // i guess this should be done more gracefully?
+          res->end(nullptr, 0);
+        }
+      });
+
+      h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+        std::cout << "Connected!!!" << std::endl;
+      });
+
+      h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+        ws.close();
+        std::cout << "Disconnected" << std::endl;
+      });
+
+      int port = 4567;
+      if (h.listen(port))
+      {
+        std::cout << "Listening to port " << port << std::endl;
+      }
+      else
+      {
+        std::cerr << "Failed to listen to port" << std::endl;
+        return -1;
+      }
+      h.run();
+  }
 }
